84e01db26a038d1e5a01f2db764348e20a3c059a
Mikki Mesfin <mikki.mesfi@gmail.com>
2017-07-14 15:31:51 -0600

    first part of calculate problem

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ app.get("/math/pi", function(request, response) {
   var pi= String(Math.PI)
   response.send(pi)
 })
+
+app.get("/math/calculate", function(request, response) {
+console.log(request.query)
+var operation = request.query.operation
+var x = parseInt(request.query.x)
+var y = parseInt(request.query.y)
+if (operation == "add") {
+  response.send(String(x+y))
+}
+console.log(x+y)
+})
 app.listen(8080)
+
+module.exports = {
+  app: app
+}
